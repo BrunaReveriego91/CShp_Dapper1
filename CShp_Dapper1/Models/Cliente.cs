3784b82222a7644ec528a1abc2d57b9dbb901b56
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-09-15 11:40:23 -0300

    Add project files.

---
+++ CShp_Dapper1/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace CShp_Dapper1.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+        public string Pais { get; set; }
+    }
+}
